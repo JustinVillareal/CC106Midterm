83bd87e7da1b20978467ff0503de4465c93a1e18
JustinVillareal <113955575+JustinVillareal@users.noreply.github.com>
2022-10-28 09:55:31 +0800

    a

--- CC106MidtermExam/src/app/app-routing.module.ts
+++ CC106MidtermExam/src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'play',
+    loadChildren: () => import('./play/play.module').then( m => m.PlayPageModule)
+  },
 ];
 
 @NgModule({
